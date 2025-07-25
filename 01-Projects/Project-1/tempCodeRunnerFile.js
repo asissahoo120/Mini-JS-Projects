9573809eb0be0c78f5b4cdf11645babbd35e7de4
ASISDEV <asissahoo120@gmail.com>
2025-07-25 23:19:13 +0530

    Delete 01-Projects directory

--- 01-Projects/Project-1/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-let bArr = Array.from("1234567890ABCDEF")
-let bgc = ""
-for (let i = 0; i < 6; i++) {
-    const element = bArr[Math.floor(Math.random()*16)];
-    bgc += element
-}
-console.log(bgc);
-// console.log(bArr.length);
--- 01-Projects/Project-3/script.js
+++
@@ -1,6 +0,0 @@
-let timeContainer = document.querySelector(".time")
-
-setInterval(() => {
-    let date = new Date()
-    timeContainer.textContent = date.toLocaleTimeString()
-}, 1000);
